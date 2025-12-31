f66a189fa4834662ff332d4205f2f3156dc73e19
aalberty <46965141+aalberty@users.noreply.github.com>
2025-12-31 19:11:06 +0000

    .

--- tests.py
+++ tests.py
@@ -79,6 +79,10 @@ TEST_CASES = {
             '== != <= >=',
             '== != <= >='
         ],
+        [
+            "function let var const if for else return",
+            "FUNCTION LET VAR CONST IF FOR ELSE RETURN"
+        ]
     ]
 }
 
@@ -97,8 +101,7 @@ def run_cases(verbose: bool):
     test_func = False
 
     tests = list(TEST_CASES)    
-    print(f"{padding}{padding}")
-    print(f"running tests: {list(tests)}")
+    print(f"\nrunning tests: {list(tests)}")
     print(f"{padding}{padding}")
 
     for test in tests:
@@ -135,10 +138,8 @@ def run_cases(verbose: bool):
                 scorecard['fails'] = scorecard['fails'] + 1
                 print(f"{scorecard['test_name']} Test {case_counter}/{len(cases)} failed -- res: '{res}'   case: '{case[0]}'    expected: '{case[1]}'")
         # TODO: change this to be a suite summary with an optional test summary after each individual test if `verbose=True`
-        print(f"{padding}Tests complete. {scorecard['passes']}/{scorecard['test_cases']} passing tests.{padding}Scorecard: {scorecard}")
         print(f"{padding}{padding}")
-        print(f"{padding}{padding}")
-            
+        print(f"Tests complete. {scorecard['passes']}/{scorecard['test_cases']} passing tests.{padding}Scorecard: {scorecard}")
     return
 
 
